218c890861171aa96bf9ce428b6b65ee9d181456
Lakshay Kalbhor <lakshay@Lakshays-MacBook-Pro.local>
2017-01-22 01:24:39 +0530

    framed response

--- Quora.py
+++ Quora.py
@@ -27,7 +27,6 @@ class ScrapeQuora:
     def links(self):
         """
         Uses the feed div to find links to :
-        0. Profile
         1. Answers
         2. Questions
         3. Activity
@@ -45,7 +44,7 @@ class ScrapeQuora:
             url_list.append("https://www.quora.com" + url.get('href'))
 
         links = {
-         'profile' : url_list[0],
+         'answers' : url_list[0],
          'questions' : url_list[1],
          'posts' : url_list[2],
          'activity': url_list[3],
@@ -73,7 +72,7 @@ class ScrapeQuora:
                 count_list.append(count.get_text())
 
         counts = {
-         'profile' : count_list[0],
+         'answers' : count_list[0],
          'questions' : count_list[1],
          'posts' : count_list[2],
          'followers': count_list[3],
@@ -85,9 +84,9 @@ class ScrapeQuora:
 
     def questions(self):
         """
-        Returns the recent questions asked by a user.
+        Returns the recent questions asked by a user and their links.
 
-        (div soup) -> list(questions) 
+        (div soup) -> list(questions), list(questions_links) 
         """
 
         links = self.links()
@@ -108,13 +107,74 @@ class ScrapeQuora:
             questions_links.append("https://www.quora.com" + link.get('href'))
 
         return questions, questions_links
-	
+
+
+    def posts(self):
+        """
+        Returns the recent questions answered by as user and their links.
+
+        (div soup) -> list(questions), list(questions_links)
+        """
+        links = self.links()
+        url = links['answers']
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        div_posts = soup.find_all('span',{'class' : 'rendered_qtext'})
+        a_links = soup.find_all('a', {'class' : 'BoardItemTitle'})
+        span_upvotes = soup.find_all('div', {'class' : 'Answer ActionBar'}) ####
+
+        posts = []
+        posts_links = []
+        post_upvotes = []
+
+        for i in range(1,len(div_posts)):
+            posts.append(div_posts[i].get_text())
+
+        for link in a_links:
+            posts_links.append("https://www.quora.com" + link.get('href'))
+
+        return posts, posts_links, post_upvotes
+
+
+
+    def answers(self):
+        """
+        Returns the recent questions answered by as user and their links.
+
+        (div soup) -> list(questions), list(questions_links)
+        """
+        links = self.links()
+        url = links['answers']
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        div_questions = soup.find_all('span',{'class' : 'question_text'})
+        a_links = soup.find_all('a', {'class' : 'question_link'})
+        span_upvotes = soup.find_all('div', {'class' : 'Answer ActionBar'}) ####
+
+        answers = []
+        answers_links = []
+        answer_upvotes = []
+
+        for question in div_questions:
+            answers.append(question.get_text())
+
+        for link in a_links:
+            answers_links.append("https://www.quora.com" + link.get('href'))
+
+        return answers, answers_links, answer_upvotes
+
 
 if __name__ == '__main__':
 
-    user = ScrapeQuora('')
+    user = ScrapeQuora('User')
     links = user.links()
     recent_questions, questions_links = user.questions()
+    recent_answers, answers_links, answer_upvotes = user.answers()
+    recent_posts, posts_links, post_upvotes = user.posts()
     feed_count = user.count()
 
 
@@ -125,4 +185,30 @@ if __name__ == '__main__':
     'recentlinks' : questions_links,
     }
 
-    pprint.pprint(questions)
+    answers = {
+    'link' : links['answers'],
+    'count' : feed_count['answers'],
+    'recent' : recent_answers,
+    'recentlinks' : answers_links,
+    'upvotes': answers_upvotes, ## Find a way to get upvotes
+    }
+
+    followers = {
+    'link' : links['followers'],
+    'count' : feed_count['followers']
+    }
+
+    following = {
+    'link' : links['following'],
+    'count' : feed_count['following']
+    }
+
+    posts = {
+    'link' : links['posts'],
+    'count' : feed_count['posts'],
+    'recent' : recent_posts, ## Needs to be looked upon
+    'recentlinks' : posts_links, ## Direct consequence of recent_posts
+    'upvotes' : posts_upvotes, ## Find a way to get upvotes 
+    }
+
+    pprint.pprint(posts)
